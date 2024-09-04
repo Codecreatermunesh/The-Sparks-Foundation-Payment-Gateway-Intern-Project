13ffa2509d34f64936a9d74ec6a676731ce92d8f
Munesh Meena <110055748+Codecreatermunesh@users.noreply.github.com>
2024-09-04 11:49:03 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,3 +4,5 @@ function toggleMenu() {
     toggleMenu.classList.toggle('active');
     navigation.classList.toggle('active');
 }
+//Toggling is typically implemented using JavaScript to add or remove a CSS class from the menu element. 
+//This class might control whether the menu is visible or hidden
